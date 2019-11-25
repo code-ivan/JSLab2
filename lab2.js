29e7a1ca0d633e0a00cb2353301084fb74c51fb5
Ivan <i.i.nazarenko02@gmail.com>
2019-11-25 15:09:41 +0200

    Lab without hash table

---
+++ lab2.js
@@ -0,0 +1,71 @@
+//task 1
+addClass(obj, 'new');
+addClass(obj, 'open');
+addClass(obj, 'me');
+
+console.log( obj.className );
+
+//task 2
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
+
+//task 3
+removeClass(obj, 'open');
+console.log( obj.className );
+removeClass(obj, 'blabla');
+console.log( obj.className );
+
+//task 4
+const arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4);
+console.log( arr );
+
+//task 5
+const arr1 = [1, 6, 5, 2, 7, 5, 1, 4, 3, 9, 8, 11, 10, 18];
+let num = 5;
+console.log(findCouple(arr1, num));
+
+//task 6
+const words1 = ['Капуста', 'Капітал'];
+const words2 = ['Ракета', 'Ракушка', 'Рак'];
+
+console.log(startingSubstring(words1));
+console.log(startingSubstring(words2));
+
+//task 7
+let arrForSort = [1, 2, 3, 4, 5];
+arrForSort.sort(function () {
+    return 0.5 - Math.random();
+});
+console.log(arrForSort);
+
+//task 8
+const john = { name: "John", age: 23 };
+const bill = { name: "Bill", age: 18 };
+const donald = { name: "Donald", age: 6 };
+
+const people = [ john, bill , donald ];
+
+people.sort(function (a, b) {
+    if(a.age > b.age){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+});
+console.log(people[0].age);
+
+//task 9
+const strings = ["C++", "C#", "C++", "C#",
+    "C", "C++", "JavaScript", "C++", "JavaScript"
+];
+
+console.log( unique(strings) );
+
+//task 10
+const auth = new Author('Joe', 'Dou', 'Writer');
+const journal = new Journal(2001, 'Forbes', 27, 10);
+const article = new Article('any', 'Random', auth, journal );
+console.log(article);
---
+++ lab2lib.js
@@ -0,0 +1,102 @@
+const obj = {
+    className: 'open menu',
+};
+function addClass(obj, text){
+    const arr = obj.className.split(" ");
+    if(arr.indexOf(text) <= 0){
+        arr.push(text);
+        obj.className = arr.join(" ");
+    }
+}
+function removeClass(obj, text){
+    const arr = obj.className.split(" ");
+    delete arr[arr.indexOf(text)];
+    obj.className = arr.join(" ");
+}
+function camelize(str) {
+    return str.replace(/\W+(.)/g, function(match, chr)
+    {
+        return chr.toUpperCase();
+    });
+}
+function filterRangeInPlace(arr, from, to ) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < from || arr[i] > to) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
+function findCouple(arr, number) {
+    let couples = [];
+    let couple = {};
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr[i];
+        couple[str] = true;
+    }
+    arr = Object.keys(couple);
+    let firstNum = null, secondNum = null;
+    arr.forEach(function (i, value) {
+        if(number - value < number) {
+            firstNum = number - value;
+            arr.forEach(function (j, value2) {
+                if(firstNum === value2 && j > i) {
+                    secondNum = value;
+                    couples.push("Пара чисел: " + secondNum + " + " + firstNum);
+                }
+            })
+        }
+    });
+    return couples;
+}
+function startingSubstring(arr) {
+    let result = "";
+    let compareString = arr[0];
+    let counter = null;
+    for(let i = 0; i < arr[0].length; i++){
+        for(let j = 0; j < arr.length; j++){
+            let str = arr[j];
+            if(compareString[i] === str[i]){
+                counter++;
+            }
+        }
+        if(counter === arr.length){
+            result += compareString[i];
+            counter = 0;
+        }
+        else {
+            return result;
+        }
+    }
+    return result;
+}
+function unique(arr) {
+    let result = [];
+    for (let item of arr) {
+        if (!result.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+function Author(name, lastName, job){
+    this.name = name;
+    this.lastName = lastName;
+    this.job = job;
+};
+function Journal(year, name, number, page){
+    this.year = year;
+    this.name = name;
+    this.number = number;
+    this.page = page;
+};
+function Article(udk, name, author, journal){
+    this.udk = udk;
+    this.name = name;
+    this.author = author;
+    this.journal = journal;
+};
+
+
+
+
